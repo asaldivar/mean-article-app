cd98acc2e20d69dd1178edc24464c37481eeae73
alexander <ag.saldivar@gmail.com>
2015-10-19 18:44:16 -0700

    configure dynamic app environment and connect to mongo

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
-var port = 3000;
+// pull correct settings per environment
+var config = require('./app/config/config.js')
+var environmentSettings = config.config();
+
+// connect to DB which is on port 27017
+mongoose.connect(environmentSettings.db);
+
+// listen to port as defined or default 3000
+var port = process.env.PORT || 3000;
 
 app.get('/', function(request, response) {
   response.json({message: 'this is json'});
