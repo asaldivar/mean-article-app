40b1d0d834cdafb33a1263da76d86d169ddaf8be
alexander <ag.saldivar@gmail.com>
2015-10-19 18:57:55 -0700

    create RESTful API and configure related middleware

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser'); // express is an extremely minimalist framework so we need body-parser to help us handle req.body
+var apiRouter = require('./app/config/routes'); // bring in API routes
+
+// configure body-parser so we can work with request.body
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // pull correct settings per environment
 var config = require('./app/config/config.js')
@@ -9,12 +15,12 @@ var environmentSettings = config.config();
 // connect to DB which is on port 27017
 mongoose.connect(environmentSettings.db);
 
+// apply router middleware
+// namespace your api
+app.use('/api', apiRouter);
+
 // listen to port as defined or default 3000
 var port = process.env.PORT || 3000;
-
-app.get('/', function(request, response) {
-  response.json({message: 'this is json'});
-});
 
 app.listen(port);
 
